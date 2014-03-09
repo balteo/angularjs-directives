5535e6581eca06aa3de83e7604b35b660565481b
balteo <balteo@gmail.com>
2014-03-09 14:47:52 +0100

    fix

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,7 +3,7 @@
 angular.module('myApp.directives', [])
     .directive('cmp', function () {
         return {
-            restrict: 'E',
+            restrict: 'A',
             controller: 'cmpCtrl',
             replace: true,
             transclude: true,
@@ -16,14 +16,14 @@ angular.module('myApp.directives', [])
     .controller('cmpCtrl', ['$scope', '$element', '$attrs' , function ($scope, $element, $attrs) {
         $scope.$parent.$watch('name', function (newVal) {
             if (newVal) {
+                console.log('$scope.$id*: '+$scope.$id);
                 $scope.$parent.updatedSize = newVal.length;
-                console.log(newVal.length);
             }
         }, true);
     }])
     .directive('enhancedTextarea', function () {
         return {
-            restrict: 'E',
+            restrict: 'A',
             replace: true,
             transclude: true,
             template: '<textarea ng-transclude></textarea>'
@@ -31,7 +31,7 @@ angular.module('myApp.directives', [])
     })
     .directive('notice', function () {
         return {
-            restrict: 'E',
+            restrict: 'A',
             require: '^cmp',
             replace: true,
             scope: {
@@ -39,9 +39,9 @@ angular.module('myApp.directives', [])
             },
             template: '<div>{{size}}</div>',
             link: function ($scope, $element, $attrs, cmpCtrl) {
-                console.log(cmpCtrl);
                 $scope.$parent.$watch('updatedSize', function (newVal) {
                     if (newVal) {
+                        console.log('$scope.$id**: '+$scope.$id);
                         $scope.size = newVal;
                     }
                 }, true);
